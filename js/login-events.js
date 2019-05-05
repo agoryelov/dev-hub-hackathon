f59cb40ebbf88f3739d72dd8bfef71956d1ab31b
esuom1 <yerdna.voleyrog@gmail.com>
2019-05-05 12:28:20 -0700

    header updated

---
+++ js/login-events.js
@@ -0,0 +1,53 @@
+    
+(function () {
+  // Initialize the FirebaseUI Widget using Firebase.
+  var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+  // User successfully signed in.
+  // Return type determines whether we continue the redirect automatically
+  // or whether we leave that to developer to handle.
+  var uiConfig = {
+    callbacks: {
+      signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+        $("#lean_overlay").fadeOut(200);
+        $("#login-dialog").css('display', 'none');
+        return true;
+      }
+    },
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+    signInFlow: 'popup',
+    signInSuccessUrl: '#',
+    signInOptions: [
+      //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ]
+  };
+
+  // The start method will wait until the DOM is loaded.
+  ui.start('#firebaseui-auth-container', uiConfig);
+})();
+
+(function (){
+    firebase.auth().onAuthStateChanged(function (user) {
+      if (user) {
+        //Logged In...
+        var promise = firebase.database().ref("users/" + user.uid).update({
+          "name": user.displayName,
+          "email": user.email
+        });
+        $('#login-anchor').hide();
+        $('#logout-anchor').show();
+      } else {
+        //Logged Out...
+        $('#logout-anchor').hide();
+        $('#login-anchor').show();
+      }
+    });
+  })();
+
+  (function () {
+    $('#logout-anchor').click(function () {
+      firebase.auth().signOut();
+    });
+  })();
---
+++ js/setup-firebase.js
@@ -0,0 +1,12 @@
+(function () {
+    var config = {
+        apiKey: "AIzaSyAtOjKoB2PcrMeQRBhzB_MCGoyKbEfTN-M",
+        authDomain: "restyle-95b83.firebaseapp.com",
+        databaseURL: "https://restyle-95b83.firebaseio.com",
+        projectId: "restyle-95b83",
+        storageBucket: "restyle-95b83.appspot.com",
+        messagingSenderId: "348022138181",
+        appId: "1:34802213811:web:3d4d8f4962aa2590"
+    };
+    firebase.initializeApp(config);
+})();
