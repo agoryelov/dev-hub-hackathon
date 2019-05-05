0a7db62e9fd0a096261e438c8775c1991bc41ef0
Edwin-Pau <pau.edwin@live.ca>
2019-05-05 14:23:31 -0700

    Edwin's fixes

--- closet_page.js
+++ closet_page.js
@@ -2,50 +2,46 @@
           subscribedItemsButtons();
       }
 
-      const subscribedItemsButtons = () => {
-          let itemCounter = 0;
-          let subscribeCounter = 5;
+      var itemCounter = 0;
+      var subscribeCounter = 5;
 
+      const subscribedItemsButtons = () => {
           let return1 = document.getElementById('item-1-return-button');
           return1.onclick = () => {
             $("#item-1-card").insertBefore("#return-bin-container div:first-child");
-            subscribeCounter--;
-            itemCounter++;
-            document.getElementById('return-bin-text').innerHTML = 'Return Bin: ' + '(' + itemCounter + ') items';
-            document.getElementById('subscribe-bin-text').innerHTML = 'Return Bin: ' + '(' + itemCounter + ') items';
+            addItem();
           }
         
           let return2 = document.getElementById('item-2-return-button');
           return2.onclick = () => {
             $("#item-2-card").insertBefore("#return-bin-container div:first-child");
-            itemCounter++;
-            document.getElementById('return-bin-text').innerHTML = 'Return Bin: ' + '(' + itemCounter + ') items';
+            addItem();
           }
 
           let return3 = document.getElementById('item-3-return-button');
           return3.onclick = () => {
             $("#item-3-card").insertBefore("#return-bin-container div:first-child");
-            itemCounter++;
-            document.getElementById('return-bin-text').innerHTML = 'Return Bin: ' + '(' + itemCounter + ') items';
+            addItem();
           }
 
           let return4 = document.getElementById('item-4-return-button');
           return4.onclick = () => {
             $("#item-4-card").insertBefore("#return-bin-container div:first-child");
-            itemCounter++;
-            document.getElementById('return-bin-text').innerHTML = 'Return Bin: ' + '(' + itemCounter + ') items';
+            addItem();
           }
 
           let return5 = document.getElementById('item-5-return-button');
           return5.onclick = () => {
             $("#item-5-card").insertBefore("#return-bin-container div:first-child");
-            itemCounter++;
-            document.getElementById('return-bin-text').innerHTML = 'Return Bin: ' + '(' + itemCounter + ') items';
+            addItem();
           }
       }
 
-      const addItem = () => {
-
+      addItem = () => {
+          subscribeCounter--;
+          itemCounter++;
+          document.getElementById('return-bin-text').innerHTML = 'Return Bin: ' + '(' + itemCounter + ') item(s)';
+          document.getElementById('subscribe-item-text').innerHTML = 'Current Subscribed Items: ' + '(' + subscribeCounter + ') of 5 total';
       }
       
       const createContainersNodes = () => {
